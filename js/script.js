eecccc7f80d40c8c2f5985fb9ec34b48abd20c5e
Marcin Pankiewicz <pankiewicz.marcin@gmail.com>
2021-04-29 17:59:00 +0200

    changed js file

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-console.log("Hello Developer :) Enjoy with doing mess on my page :)");
+{
+    const hideShowTableButton = document.querySelector(".js-hideShowTableButton");
+    const hideShowContactButton = document.querySelector(".js-hideShowContactButton");
+    const hideShowImageButton = document.querySelector(".js-hideShowImageButton");
 
-const hideShowImageButton = document.querySelector(".js-hideShowImageButton");
-const hideShowTableButton = document.querySelector(".js-hideShowTableButton");
-const hideShowContactButton = document.querySelector(".js-hideShowContactButton");
-const image = document.querySelector(".js-section__image");
-const musicTable = document.querySelector(".js-tableContainer");
-const contact = document.querySelector(".js-contact")
+    const onClickHideShowImage = () => {
+        const image = document.querySelector(".js-section__image");
+        image.classList.toggle("section__imageOff");
+        hideShowImageButton.innerText = image.classList.contains("section__imageOff") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
+    };
+    const onClickHideShowTable = () => {
+        const musicTable = document.querySelector(".js-tableContainer");
+        musicTable.classList.toggle("tableOff");
+        hideShowTableButton.innerText = musicTable.classList.contains("tableOff") ? "Pokaż tabelę" : "Ukryj tabelę";
+    };
+    const onClickHideShowContact = () => {
 
-hideShowImageButton.addEventListener("click", () => {
-    image.classList.toggle("section__imageOff");
-    hideShowImageButton.innerText = image.classList.contains("section__imageOff") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
-});
-hideShowTableButton.addEventListener("click", () => {
-    musicTable.classList.toggle("tableOff");
-    hideShowTableButton.innerText = musicTable.classList.contains("tableOff") ? "Pokaż tabelę" : "Ukryj tabelę";
-});
-hideShowContactButton.addEventListener("click", () => {
-    contact.classList.toggle("section__contactOff");
-    hideShowContactButton.innerText = contact.classList.contains("section__contactOff") ? "Pokaż kontakt" : "Ukryj kontakt";
-});
+        const contact = document.querySelector(".js-contact");
+        contact.classList.toggle("section__contactOff");
+        hideShowContactButton.innerText = contact.classList.contains("section__contactOff") ? "Pokaż kontakt" : "Ukryj kontakt";
+    };
+
+    const init = () => {
+        console.log("Hello Developer :) Enjoy with doing mess on my page :)");
+
+        hideShowImageButton.addEventListener("click", onClickHideShowImage);
+
+        hideShowTableButton.addEventListener("click", onClickHideShowTable);
+
+        hideShowContactButton.addEventListener("click", onClickHideShowContact);
+    }
+    init();
+}
